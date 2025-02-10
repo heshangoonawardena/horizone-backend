e2a615c92c15f203f60eda77a50a049b873c70fc
heshangoonawardena <heshangoonawardena@gmail.com>
2025-02-10 13:13:18 +0530

    session 7

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,10 +4,12 @@ import hotelsRouter from "./api/hotel.js";
 import connectDB from "./infrastructure/db.js";
 import userRouter from "./api/user.js";
 import bookingRouter from "./api/booking.js";
+import cors from "cors"
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 connectDB()
 
@@ -18,7 +20,3 @@ app.use("/api/bookings", bookingRouter)
 
 // Define the port to run the server
 app.listen(3000, console.log("server listening on port 3000"));
-
-
-// mongodb + srv://heshangoonawardena:<db_password>@cluster0.gnmpz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-// 0P1wiX5s83pXNein
